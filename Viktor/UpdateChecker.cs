5c2cbf29839074fdbb1ac96b74bf14a1aaa84088
Apollo <jango00@hotmail.de>
2015-04-21 17:52:38 +0200

    Updated Viktor

--- Viktor/UpdateChecker.cs
+++ Viktor/UpdateChecker.cs
@@ -16,7 +16,7 @@ namespace Viktor
                 var request =
                     WebRequest.Create(
                         String.Format(
-                            "https://raw.githubusercontent.com/{0}/LeagueSharp/master/{1}/Properties/AssemblyInfo.cs",
+                            "https://raw.githubusercontent.com/{0}/LeagueSharp/master/{1}/AssemblyInfo.cs",
                             user, assembly));
                 var response = request.GetResponse();
                 var data = response.GetResponseStream();
